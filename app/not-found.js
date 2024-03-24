dae112b110c4f7fbc33686f173788dc501952e6d
jerin <jerinkunnamkada@gmail.com>
2024-03-24 15:26:47 +0530

    redirect funtction

---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="text-4xl h-screen font-bold grid place-items-center">
+      {" "}
+      404 Not Found
+    </div>
+  );
+};
+
+export default NotFound;
