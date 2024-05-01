9c8d847bc4f484d0b227f0f357edfe6a7fe7a2d1
jerin <jerinkunnamkada@gmail.com>
2024-05-01 11:39:43 +0530

    todo app

--- app/page.js
+++ app/page.js
@@ -1,31 +1,63 @@
-"use client";
-import { useRouter } from "next/navigation";
+import React from "react";
 
 const Home = () => {
-  const router = useRouter();
-
-  const navigate = (page) => {
-    router.push(page);
-  };
-
   return (
-    <section>
-      <h1>Use Router</h1>
-      <button
-        className="bg-orange-400 rounded-2xl mx-5 px-5 py-3"
-        onClick={() => navigate("about")}
-      >
-        
-        Go to about page
-      </button>
-      <button
-        className="bg-orange-400 rounded-2xl mx-5 px-5 py-3"
-        onClick={() => navigate("login")}
-      >
-        
-        Go to login page
-      </button>
-    </section>
+    <>
+      <form className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter Title"
+          className="px-3 py-2 border-2 w-full"
+        />
+        <textarea
+          name="description"
+          placeholder="Enter Description "
+          className="px-3 py-2 border-2 w-full"
+        ></textarea>
+        <button type="submit" className="bg-orange-600 py-3 px-11 text-white">
+          Add Todo
+        </button>
+      </form>
+      <div className="relative overflow-x-auto mt-24 w-[60%] mx-auto">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Id
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Title
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Description
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Status
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Apple MacBook Pro 17"
+              </th>
+              <td className="px-6 py-4">Silver</td>
+              <td className="px-6 py-4">Laptop</td>
+              <td className="px-6 py-4">$2999</td>
+            </tr>
+            
+          </tbody>
+        </table>
+      </div>
+      ;
+    </>
   );
 };
 
