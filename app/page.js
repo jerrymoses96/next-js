18087a3185777c4a98fd6d99f30b0220b913cde3
jerin <jerinkunnamkada@gmail.com>
2024-05-02 17:42:38 +0530

    updated

--- app/page.js
+++ app/page.js
@@ -52,7 +52,17 @@ const Home = () => {
               <td className="px-6 py-4">Laptop</td>
               <td className="px-6 py-4">$2999</td>
             </tr>
-            
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Apple MacBook Pro 17"
+              </th>
+              <td className="px-6 py-4">Silver</td>
+              <td className="px-6 py-4">Laptop</td>
+              <td className="px-6 py-4">$2999</td>
+            </tr>
           </tbody>
         </table>
       </div>
